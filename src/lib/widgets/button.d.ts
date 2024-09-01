73b20dc275db77775ac3d3e45f60e63965a06152
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2024-08-31 17:48:40 -0700

    fix: cleanup

--- src/lib/widgets/button.d.ts
+++
@@ -1,5 +0,0 @@
-import { ClickEvents, Hovered, WidgetArguments, WidgetEvents } from "./creation/utils";
-
-type ButtonArguments = [Text: string];
-type ButtonEvents = Hovered & ClickEvents;
-export type BaseButtonDeclaration = WidgetArguments<ButtonArguments> & WidgetEvents<ButtonEvents>;
--- src/lib/widgets/format.d.ts
+++
@@ -1,7 +0,0 @@
-import { WidgetArguments } from "./creation/utils";
-
-type IndentArguments = [Width?: number];
-type SameLineArguments = [Width?: number, VerticalAlignment?: Enum.VerticalAlignment];
-
-export type IndentDeclaration = WidgetArguments<IndentArguments>;
-export type SameLineDeclaration = WidgetArguments<SameLineArguments>;
